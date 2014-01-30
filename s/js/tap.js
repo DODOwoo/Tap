31691676fbb054d8361e71592a1a4eaba9d23b19
DODOwoo <xuan0227@gmail.com>
2014-01-31 02:15:12 +0800

    split css, js to new file

---
+++ s/js/tap.js
@@ -0,0 +1,56 @@
+var forEach = function (context, fn) {
+		[].forEach.call(context, fn)
+	}
+
+function initdrag()
+{	
+	var handleStart = function (event) {
+		var style = window.getComputedStyle(event.target, null);
+			//console.log('dragstart: ', event, event.offsetX);
+		var allClass = $('#'+event.target.id).attr('class').split(' ');
+		var className = allClass[allClass.length-1];
+		event.dataTransfer.setData('selectorClass', className); 
+	};
+
+	var handleDragOver = function (event) {
+		if (event.preventDefault) {
+			event.preventDefault();
+		}
+
+		var selectors = document.querySelectorAll('.'+event.dataTransfer.getData('selectorClass'));
+		forEach(selectors, function ($dm) {
+			if(!$($dm).is('[draggable]'))
+			{
+		    	$dm.parentNode.appendChild($dm);
+				$dm.style.left = (event.offsetX + 120 - $dm.offsetWidth/2) + 'px';
+				$dm.style.top = '6px'; //(event.offsetY) + 'px';
+			}
+		})
+
+		return false;
+	};
+
+	var handleDrop = function (event) {
+		if (event.stopPropagation) {
+			event.stopPropagation();
+		}
+
+		var selectors = document.querySelectorAll('.'+event.dataTransfer.getData('selectorClass'));
+		forEach(selectors, function ($dm) {
+			$dm.parentNode.appendChild($dm);
+			$dm.style.left = (event.offsetX + 120 - $dm.offsetWidth/2) + 'px';
+			$dm.style.top = '6px'; //(event.offsetY) + 'px';
+		})
+		return false;
+	}
+
+	var plans = document.querySelectorAll('.threadrow .plan');
+	var lines = document.querySelectorAll('.threadrow');
+	forEach(plans, function ($plan) {
+		$plan.addEventListener('dragstart', handleStart, false);
+	});
+	forEach(lines, function ($line) {
+		$line.addEventListener('dragover', handleDragOver, false);
+		$line.addEventListener('drop', handleDrop, false);
+	});
+}
