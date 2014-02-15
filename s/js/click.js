b1adbbdcc876363e270b0c5fcaf207102305d87b
DODOwoo <xuan0227@gmail.com>
2014-02-16 00:23:53 +0800

    random color prefer not be duplicate in a short time

--- s/js/click.js
+++ s/js/click.js
@@ -1,5 +1,3 @@
-var backgroundcolors = ['#1ABC9C','#16A085','#2ECC71','#27AE60','#3498DB','#2980B9','#9B59B6','#8E44AD','#34495E','#2C3E50','#F1C40F','#F39C12','#E67E22','#D35400','#E74C3C','#C0392B','#ECF0F1','#BDC3C7','#95A5A6','#7F8C8D'];
-	
 function initclickevent()
 {
 	$('#btn-load-resource')[0].addEventListener('click', function (e) {
@@ -116,7 +114,7 @@ var addTaskAndChildren = function(taskmodal, source, newtaskid, newtaskleft){
 	var newtaskwidth = newtaskcompwidth + newtaskprologwidth;
 	var newtaskname = taskmodal.find(".task-name").val(); //$("#addTaskModal #task-name").val();
 	
-	var newtaskcolor = backgroundcolors[Math.round(Math.random()*20)];
+	var newtaskcolor = getRandomColor(); //backgroundcolors[Math.round(Math.random()*20)];
 	var newtaskclass = newtaskid+newtaskname.split(' ').join('');
 
 	var targetParentIds = [source[0].id];
--- s/js/tap.js
+++ s/js/tap.js
@@ -1,3 +1,5 @@
+var backgroundcolors = ['#1ABC9C','#16A085','#2ECC71','#27AE60','#3498DB','#2980B9','#9B59B6','#8E44AD','#34495E','#2C3E50','#F1C40F','#F39C12','#E67E22','#D35400','#E74C3C','#C0392B','#ECF0F1','#BDC3C7','#95A5A6','#7F8C8D'];
+
 var forEach = function (context, fn) {
 		[].forEach.call(context, fn)
 }
@@ -12,6 +14,17 @@ var convertToClock = function(width_int){
 
 var convertToWidth = function(clock){
 	return clock * 20 + 'px';
+}
+
+var selectedColorIndex = [];
+var getRandomColor = function(){
+	if(selectedColorIndex.length === 20){
+		selectedColorIndex = [];
+	}
+	for(i=Math.round(Math.random()*20); jQuery.inArray(i,selectedColorIndex) > -1; i = Math.round(Math.random()*20)){
+	}
+	selectedColorIndex.push(i);
+	return backgroundcolors[i];
 }
 
 var getMixPlanColor = function(color, width){
